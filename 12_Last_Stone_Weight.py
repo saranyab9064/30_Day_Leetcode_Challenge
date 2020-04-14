444e663852c8d97dac6005f924c54e27d7074157
SARANYA BALAKRISHNAN <55033431+saranyab9064@users.noreply.github.com>
2020-04-13 21:48:17 -0700

    Create 12_Last_Stone_Weight.py

---
+++ 12_Last_Stone_Weight.py
@@ -0,0 +1,64 @@
+
+# ============================================================================
+# Name        : 12_Last_Stone_Weight.py
+# Author      : Saranya Balakrishnan
+# Mail Id     : saranyab0925@gmail.com
+# ============================================================================
+"""
+We have a collection of stones, each stone has a positive integer weight.
+
+Each turn, we choose the two heaviest stones and smash them together.  Suppose the stones have weights x and y with x <= y.  The result of this smash is:
+
+If x == y, both stones are totally destroyed;
+If x != y, the stone of weight x is totally destroyed, and the stone of weight y has new weight y-x.
+At the end, there is at most 1 stone left.  Return the weight of this stone (or 0 if there are no stones left.)
+
+
+
+Example 1:
+
+Input: [2,7,4,1,8,1]
+Output: 1
+Explanation:
+We combine 7 and 8 to get 1 so the array converts to [2,4,1,1,1] then,
+we combine 2 and 4 to get 2 so the array converts to [2,1,1,1] then,
+we combine 2 and 1 to get 1 so the array converts to [1,1,1] then,
+we combine 1 and 1 to get 0 so the array converts to [1] then that's the value of last stone.
+
+
+Note:
+
+1 <= stones.length <= 30
+1 <= stones[i] <= 1000
+"""
+
+
+class Solution(object):
+
+
+        def lastStoneWeight(self, stones):
+            """
+            :type stones: List[int]
+            :rtype: int
+            """
+            stones = sorted(stones, reverse=True)
+            while len(stones) > 1:
+                x = stones[0]
+                y = stones[1]
+
+                stones.remove(x)
+                stones.remove(y)
+                if x != y:
+                    stones.append(x - y)
+                stones = sorted(stones, reverse=True)
+            return stones[0] if len(stones) == 1 else 0
+
+
+
+
+
+if __name__ == "__main__":
+    test = Solution()
+    arr = [2,7,4,1,8,1]
+    out = test.lastStoneWeight(arr)
+    print(out)
